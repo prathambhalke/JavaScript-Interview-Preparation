840287b02506ffe13b9031508c36eefbb60fd63d
prathambhalke <prathameshbhalke125@gmail.com>
2024-07-24 10:40:41 +0530

    To Be Or Not To Be

---
+++ leetcode-30-days-javascript/03-except.js
@@ -0,0 +1,21 @@
+// Write a function called expect to help developers test their code. This function should take any value as input and return an object with two functions:
+
+// toBe(val): This function should take another value as input and return true if both values are exactly equal (===). If they are not equal, it should throw an error with the message "Not Equal".
+
+// notToBe(val): This function should take another value as input and return true if the values are not exactly equal (!==). If they are equal, it should throw an error with the message "Equal".
+
+var expect = function(val) {
+    return {
+      toBe : (val2) =>{
+          if(val !== val2)  throw new Error("Not Equal")
+          else  return true
+      },
+        notToBe : (val2) => {
+            if(val === val2) throw new Error("Equal")
+           else return true
+        }
+    }
+};
+
+expect(5).toBe(5); // true
+expect(5).notToBe(5); // throws "Equal"
